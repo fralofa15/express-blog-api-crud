f077b80fc41d530b8ea1f2091d6d4b6e0f919c1d
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-05-29 13:54:07 +0200

    Impostato file app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+    response.send("mio server");
+});
+
+app.listen(port, () => {
+    console.log(`Server in ascolto sulla porta ${port}`);
+});
